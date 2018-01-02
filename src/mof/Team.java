7d0842f77dc9c8bb51ae959b88b7816ff3031223
Ana Arsovska <anamarsovska@gmail.com>
2018-01-02 16:34:16 +0100

    Update Team.java

--- src/mof/Team.java
+++ src/mof/Team.java
@@ -13,18 +13,90 @@ import java.util.Arrays;
  */
 public class Team {
 
-    final private String[] NAMES;
-
-    public Team(String[] names) {
-        NAMES = names;
+    final private String Name;
+    final private int index;
+    final private int SpeakerOne;
+    final private int SpeakerTwo;
+    final private int SpeakerThree;
+    private int cumulPts;
+    final private String organization;
+    private int[] Wins;
+    private int wins;
+    private double ranking;
+    
+    public Team(String name, int ind, String org) {
+        Name = name;
+        index = ind;
+        SpeakerOne = index*3-3;
+        SpeakerTwo = index*3-2;
+        SpeakerThree = index*3-1;
+        cumulPts = 0;
+        organization = org;
+        Wins = new int[9];
+        wins = 0;
+        ranking = 0.0;
     }
 
-    public String[] getNames() {
-        return NAMES;
+    public String getNames() {
+        return Name;
     }
-
-    @Override
-    public String toString() {
-        return Arrays.toString(NAMES);
+    
+    public int getIndex(){
+        return index;
     }
+    
+    public int getSpeakerOne(){
+        return SpeakerOne;
+    }
+    
+    public int getSpeakerTwo(){
+        return SpeakerTwo;
+    }
+    
+    public int getSpeakerThree(){
+        return SpeakerThree;
+    }
+    
+    public int getPts(){
+        return cumulPts;
+    }
+    
+    public double getRanking(){
+        ranking = wins*1000 + getPts();
+        return ranking;
+    }
+    
+    public String getOrg(){
+        return organization;
+    }
+    
+    public double addPts(double pts){
+        cumulPts += pts;
+        return cumulPts;
+    }
+    
+    public void addWin(int round, int win){
+        if(win == 1){
+            Wins[round-1] = 1;
+            wins++;
+        }
+        else{
+            Wins[round-1] = 0;
+        }
+            
+    }
+    
+    public void addLoss(int round){
+        Wins[round-1] = 0;
+    }
+    
+    public int getWins(){
+        return wins;
+    }
+    
+    public int getRoundResult(int round){
+        return Wins[round-1];
+    }
+    
 }
+	
