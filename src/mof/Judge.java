c60c593d375cfb6ddc550eb460977647f8b7e3a2
Ana Arsovska <anamarsovska@gmail.com>
2018-01-02 16:18:30 +0100

    Update Judge.java

--- src/mof/Judge.java
+++ src/mof/Judge.java
@@ -5,6 +5,8 @@
  */
 package mof;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ana
@@ -12,17 +14,69 @@ package mof;
 public class Judge {
 
     final private String NAME;
-
-    public Judge(String name) {
+    final private String qualYears;
+    final private String organization;
+    final private int judgeIndex;
+    private boolean hasJudged;
+    private boolean available;
+    private int qualScale;
+            
+    public Judge(String name, String qual, String org, int index, boolean hasjudged) {       
         NAME = name;
+        qualYears = qual;
+        organization = org;
+        judgeIndex = index;
+        hasJudged = hasjudged;
+        qual();
     }
-
-    public String getName() {
+    
+    public String getName(){
         return NAME;
     }
+    
+    private void qual(){
+        if(null != qualYears)switch (qualYears) {
+            case "A":
+                qualScale = 4;
+                break;
+            case "B":
+                qualScale = 3;
+                break;
+            case "C":
+                qualScale = 2;
+                break;
+            case "Shadow":
+                qualScale = 1;
+                break;
+            default:
+                qualScale = 0;
+                break;
+        }
+    }
+    
+    public int getQual(){
+        return qualScale;
+    }
+      
+    public String getOrg(){
+        return organization;
+    }
 
+    public int getIndex(){
+        return judgeIndex;
+    }
+		
+    public void markJudged(){
+        hasJudged = true;
+    }
+    
+    public void unmarkJudged(){
+        hasJudged = false;
+    }
+   
     @Override
     public String toString() {
         return NAME;
     }
 }
+
